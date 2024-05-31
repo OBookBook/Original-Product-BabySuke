c378263cbd25766c66b174b90743dba5258c6749
N.I <iworksWeb@gmail.com>
2024-05-31 17:37:44 +0900

    update | api.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,5 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    // 認証されたユーザーの情報を取得
     return $request->user();
 });
+
+// http://localhost:8080/api
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+
+require __DIR__ . '/auth.php';
