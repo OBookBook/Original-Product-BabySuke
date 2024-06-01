83e655da919e3560245dcfdc24331d452ba61977
N.I <iworksWeb@gmail.com>
2024-06-01 22:49:18 +0900

    create | ChildDailyRecord table migration

--- api/database/migrations/2024_06_01_133144_create_child_daily_records_table.php
+++ api/database/migrations/2024_06_01_133144_create_child_daily_records_table.php
@@ -12,8 +12,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('child_daily_records', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('child_id')->unsigned();
+            $table->tinyInteger('health_status')->nullable(false);
+            $table->decimal('body_temperature', 4, 2)->nullable();
+            $table->tinyInteger('stool_count')->nullable();
+            $table->boolean('morning_medication_taken')->nullable();
+            $table->boolean('afternoon_medication_taken')->nullable();
+            $table->boolean('evening_medication_taken')->nullable();
+            $table->boolean('bedtime_medication_taken')->nullable();
+            $table->text('new_ability')->nullable();
+            $table->text('comment')->nullable();
+            $table->string('image', 255)->nullable();
             $table->timestamps();
+
+            $table->foreign('child_id')->references('id')->on('childrens')->onDelete('cascade');
         });
     }
 
