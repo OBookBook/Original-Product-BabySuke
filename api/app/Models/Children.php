56a0510d93158f93d72cd65e86f55e6e94a8958f
N.I <iworksWeb@gmail.com>
2024-06-02 11:23:15 +0900

    relationships  | User, Children, and FamilyGroup

--- api/app/Models/Children.php
+++ api/app/Models/Children.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Children extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'gender', 'birth_date'];
+
+    public function familyGroups(): HasMany
+    {
+        return $this->hasMany(FamilyGroup::class, 'child_id');
+    }
 }
--- api/app/Models/FamilyGroup.php
+++ api/app/Models/FamilyGroup.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class FamilyGroup extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'child_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function child(): BelongsTo
+    {
+        return $this->belongsTo(Children::class, 'child_id');
+    }
+
 }
