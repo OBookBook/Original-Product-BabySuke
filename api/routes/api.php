2cee5d4963b90635816dab1fbbec5da3e06f37f6
N.I <iworksWeb@gmail.com>
2024-06-02 11:21:44 +0900

    add | Route::resource('family', FamilyGroupController::class);

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ChildrenController;
+use App\Http\Controllers\FamilyGroupController;
 
 // Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 //     // 認証されたユーザーの情報を取得
@@ -23,5 +24,6 @@ Route::get('/', function () {
 });
 
 Route::resource('children', ChildrenController::class);
+Route::resource('family', FamilyGroupController::class);
 
 require __DIR__ . '/auth.php';
